0edb4f76e59a9eb8fd78710482d9f957d366d3bf
Arachneee <jhg2819@naver.com>
2023-12-12 16:08:09 +0900

    feat: 노선 이름 2글자 미만 예외

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -2,13 +2,24 @@ package subway.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import subway.exception.ErrorMessage;
+import subway.exception.GameException;
 
 public class Line {
+
+    private static final int MIN_NAME_LENGTH = 2;
     private String name;
     private List<Station> stations = new ArrayList<>();
 
     public Line(String name) {
+        validate(name);
         this.name = name;
+    }
+
+    private void validate(final String name) {
+        if (name.length() < MIN_NAME_LENGTH) {
+            throw new GameException(ErrorMessage.INVALID_SIZE);
+        }
     }
 
     public String getName() {
